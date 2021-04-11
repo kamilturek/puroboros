8d3075bc890aeea47091a53d365b1cf199104894
Kamil Turek <kamil.turek@hotmail.com>
2021-04-11 23:22:41 +0200

    Implement operator precedence table

--- puroboros/expr.py
+++ puroboros/expr.py
@@ -1,41 +1,72 @@
-from .context import Context
+from typing import NoReturn
+
 from .defs import ASTNode, ASTNodeType, Token, TokenType
 from .exceptions import ParserError
 from .scan import Scanner
-from .tree import mkastleaf
 
 
-def primary(tok: Token, context: Context) -> ASTNode:
-    match tok.type:
-        case TokenType.T_INTLIT:
-            return mkastleaf(ASTNodeType.A_INTLIT, tok.intvalue)
-        case _:
-            raise ParserError(f'Syntax error on line {context.line}')
+class Parser:
+    OP_PRECEDENCE = {
+        TokenType.T_PLUS: 10,
+        TokenType.T_MINUS: 10,
+        TokenType.T_STAR: 20,
+        TokenType.T_SLASH: 20,
+    }
 
+    def __init__(self, scanner: Scanner) -> None:
+        self.scanner = scanner
+        self.context = scanner.context
 
-def arithop(tok: TokenType, context: Context) -> TokenType:
-    match tok:
-        case TokenType.T_PLUS:
-            return ASTNodeType.A_ADD
-        case TokenType.T_MINUS:
-            return ASTNodeType.A_SUBTRACT
-        case TokenType.T_STAR:
-            return ASTNodeType.A_MULTIPLY
-        case TokenType.T_SLASH:
-            return ASTNodeType.A_DIVIDE
-        case _:
-            raise ParserError(f'Unknown token in arithop() on line {context.line}')
+    def raise_syntax_error(self) -> NoReturn:
+        msg = f'Syntax error on line {self.context.line}'
+        raise ParserError(msg)
 
+    def primary(self, token: Token) -> ASTNode:
+        match token.type:
+            case TokenType.T_INTLIT:
+                return ASTNode(
+                    op=ASTNodeType.A_INTLIT,
+                    intvalue=token.intvalue,
+                )
+            case _:
+                self.raise_syntax_error()
 
-def binexpr(scanner: Scanner, context: Context) -> ASTNode:
-    left_token = scanner.scan()
-    left = primary(left_token, context)
+    def arith_op(self, token_type: TokenType) -> ASTNodeType:
+        match token_type:
+            case TokenType.T_PLUS:
+                return ASTNodeType.A_ADD
+            case TokenType.T_MINUS:
+                return ASTNodeType.A_SUBTRACT
+            case TokenType.T_STAR:
+                return ASTNodeType.A_MULTIPLY
+            case TokenType.T_SLASH:
+                return ASTNodeType.A_DIVIDE
+            case _:
+                self.raise_syntax_error()
 
-    op_token = scanner.scan()
-    if op_token.type == TokenType.T_EOF:
-        return left
-    nodetype = arithop(op_token.type, context)
+    def op_precedence(self, token_type: TokenType) -> int:
+        try:
+            return self.OP_PRECEDENCE[token_type]
+        except KeyError:
+            self.raise_syntax_error()
 
-    right = binexpr(scanner, context)
+    def bin_expr(self, precedence: int = 0) -> ASTNode:
+        left_token = self.scanner.scan()
+        left_node = self.primary(left_token)
+        op_token = self.scanner.scan()
 
-    return ASTNode(nodetype, left, right)
+        if op_token.type == TokenType.T_EOF:
+            return left_node, op_token
+
+        while self.op_precedence(op_token.type) > precedence:
+            right_node, next_op_token = self.bin_expr(self.op_precedence(op_token.type))
+            left_node = ASTNode(
+                op=self.arith_op(op_token.type),
+                left=left_node,
+                right=right_node,
+            )
+            op_token = next_op_token
+            if op_token.type == TokenType.T_EOF:
+                break
+
+        return left_node, op_token
--- tests/test_expr.py
+++ tests/test_expr.py
@@ -3,30 +3,45 @@ from unittest.mock import patch
 
 import pytest
 
-from puroboros.defs import ASTNode, ASTNodeType, Token, TokenType
 from puroboros.context import Context
+from puroboros.defs import ASTNode, ASTNodeType, Token, TokenType
 from puroboros.exceptions import ParserError
-from puroboros.expr import arithop, binexpr, primary
+from puroboros.expr import Parser
 from puroboros.scan import Scanner
 
 
+@pytest.fixture
+def parser():
+    context = Context()
+    scanner = Scanner(context)
+    parser = Parser(scanner)
+    return parser
+
+
+class TestParserSyntaxError:
+    def test_raises_syntax_error(self, parser):
+        parser.context.line = 10
+        with pytest.raises(ParserError) as e:
+            parser.raise_syntax_error()
+        
+        assert str(e.value) == 'Syntax error on line 10'
+
+
 class TestPrimary:
-    def test_primary_success(self):
-        context = Context()
+    def test_primary_success(self, parser):
         token = Token(TokenType.T_INTLIT, 5)
-        node = primary(token, context)
+        node = parser.primary(token)
 
         assert node.op == ASTNodeType.A_INTLIT
         assert node.intvalue == 5
         assert node.left is None
         assert node.right is None
 
-    def test_primary_failure(self):
-        context = Context()
+    def test_primary_failure(self, parser):
         token = Token(TokenType.T_STAR)
 
         with pytest.raises(ParserError) as e:
-            primary(token, context)
+            parser.primary(token)
 
         assert str(e.value) == 'Syntax error on line 1'
 
@@ -41,43 +56,39 @@ class TestArithmeticOperator:
             (TokenType.T_SLASH, ASTNodeType.A_DIVIDE),
         ]
     )
-    def test_arithop_success(self, token_type, expected_node_type):
-        node_type = arithop(token_type, Context())
+    def test_arithop_success(self, parser, token_type, expected_node_type):
+        node_type = parser.arith_op(token_type)
 
         assert node_type == expected_node_type
 
-    def test_arithop_failure(self):
+    def test_arithop_failure(self, parser):
         with pytest.raises(ParserError) as e:
-            arithop(TokenType.T_EOF, Context())
+            parser.arith_op(TokenType.T_EOF)
 
-        assert str(e.value) == 'Unknown token in arithop() on line 1'
+        assert str(e.value) == 'Syntax error on line 1'
 
 
 class TestBinaryExpression:
-    def test_one_level(self):
-        """
-        5
-        """
-        context = Context()
-        scanner = Scanner(context)
-        with patch.object(context, 'infile', StringIO('5')):
-            node = binexpr(scanner, context)
-
+    def test_int_literal(self, parser):
+        with patch.object(parser.context, 'infile', StringIO('1')):
+            node, op_token = parser.bin_expr()
+    
         assert node == ASTNode(
             op=ASTNodeType.A_INTLIT,
-            intvalue=5,
+            intvalue=1,
+        )
+        assert op_token == Token(
+            type=TokenType.T_EOF,
         )
 
-    def test_two_level(self):
+    def test_additive_expr(self, parser):
         """
           +
          / \
-        1   3
+        1   2
         """
-        context = Context()
-        scanner = Scanner(context)
-        with patch.object(context, 'infile', StringIO('1 + 3')):
-            node = binexpr(scanner, context)
+        with patch.object(parser.context, 'infile', StringIO('1 + 2')):
+            node, op_token = parser.bin_expr()
 
         assert node == ASTNode(
             op=ASTNodeType.A_ADD,
@@ -87,47 +98,129 @@ class TestBinaryExpression:
             ),
             right=ASTNode(
                 op=ASTNodeType.A_INTLIT,
+                intvalue=2,
+            ),
+        )
+        assert op_token == Token(
+            type=TokenType.T_EOF,
+        )
+
+    def test_double_additive_expr(self, parser):
+        """
+            +
+           / \
+          +   3
+         / \
+        1   2
+        """
+        with patch.object(parser.context, 'infile', StringIO('1 + 2 + 3')):
+            node, op_token = parser.bin_expr()
+        
+        assert node == ASTNode(
+            op=ASTNodeType.A_ADD,
+            left=ASTNode(
+                op=ASTNodeType.A_ADD,
+                left=ASTNode(
+                    op=ASTNodeType.A_INTLIT,
+                    intvalue=1,
+                ),
+                right=ASTNode(
+                    op=ASTNodeType.A_INTLIT,
+                    intvalue=2,
+                ),
+            ),
+            right=ASTNode(
+                op=ASTNodeType.A_INTLIT,
                 intvalue=3,
             ),
         )
+        assert op_token == Token(
+            type=TokenType.T_EOF,
+        )
 
-    def test_four_level(self):
+    def test_multiplicative_expr(self, parser):
         """
           *
          / \
-        2   +
-           / \
-          3   *
-             / \
-            4   5
+        1   2
         """
-        context = Context()
-        scanner = Scanner(context)
-        with patch.object(context, 'infile', StringIO('2 * 3 + 4 * 5')):
-            node = binexpr(scanner, context)
+        with patch.object(parser.context, 'infile', StringIO('1 * 2')):
+            node, op_token = parser.bin_expr()
 
         assert node == ASTNode(
             op=ASTNodeType.A_MULTIPLY,
             left=ASTNode(
                 op=ASTNodeType.A_INTLIT,
-                intvalue=2,
+                intvalue=1,
             ),
             right=ASTNode(
-                op=ASTNodeType.A_ADD,
+                op=ASTNodeType.A_INTLIT,
+                intvalue=2,
+            ),
+        )
+        assert op_token == Token(
+            type=TokenType.T_EOF,
+        )
+
+    def test_double_multiplicative_expr(self, parser):
+        """
+            *
+           / \
+          *   3
+         / \
+        1   2
+        """
+        with patch.object(parser.context, 'infile', StringIO('1 * 2 * 3')):
+             node, op_token = parser.bin_expr()
+
+        assert node == ASTNode(
+            op=ASTNodeType.A_MULTIPLY,
+            left=ASTNode(
+                op=ASTNodeType.A_MULTIPLY,
                 left=ASTNode(
                     op=ASTNodeType.A_INTLIT,
-                    intvalue=3,
+                    intvalue=1,
                 ),
                 right=ASTNode(
-                    op=ASTNodeType.A_MULTIPLY,
-                    left=ASTNode(
-                        op=ASTNodeType.A_INTLIT,
-                        intvalue=4,
-                    ),
-                    right=ASTNode(
-                        op=ASTNodeType.A_INTLIT,
-                        intvalue=5,
-                    ),
+                    op=ASTNodeType.A_INTLIT,
+                    intvalue=2,
                 ),
             ),
+            right=ASTNode(
+                op=ASTNodeType.A_INTLIT,
+                intvalue=3
+            ),
+        )
+        assert op_token == Token(
+            type=TokenType.T_EOF,
+        )
+
+    def test_mixed_expr(self, parser):
+        """
+            +
+           / \
+          *   3
+         / \
+        1   2
+        """
+        with patch.object(parser.context, 'infile', StringIO('1 * 2 + 3')):
+            node, op_token = parser.bin_expr()
+
+        assert node == ASTNode(
+            op=ASTNodeType.A_ADD,
+            left=ASTNode(
+                op=ASTNodeType.A_MULTIPLY,
+                left=ASTNode(
+                    op=ASTNodeType.A_INTLIT,
+                    intvalue=1,
+                ),
+                right=ASTNode(
+                    op=ASTNodeType.A_INTLIT,
+                    intvalue=2,
+                ),
+            ),
+            right=ASTNode(
+                op=ASTNodeType.A_INTLIT,
+                intvalue=3,
+            )
         )
