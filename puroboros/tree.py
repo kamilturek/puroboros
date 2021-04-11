b1f5f4aeff445bd0c579aa9e7589625ee39af65b
Kamil Turek <kamil.turek@hotmail.com>
2021-04-11 23:26:26 +0200

    Remove unused tree functions

--- puroboros/tree.py
+++
@@ -1,9 +0,0 @@
-from .defs import ASTNode, ASTNodeType
-
-
-def mkastleaf(op: ASTNodeType, intvalue: int) -> ASTNode:
-    return ASTNode(op=op, intvalue=intvalue)
-
-
-def mkastunary(op: ASTNodeType, left: ASTNode, intvalue: int) -> ASTNode:
-    return ASTNode(op=op, left=left, intvalue=intvalue)
