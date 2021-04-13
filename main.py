0b03c0c034f5e812cf37dabbfc62eb7a93fd8c2c
Kamil Turek <kamil.turek@hotmail.com>
2021-04-13 19:41:02 +0200

    Adjust main.py to use interpreter

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
+#!/usr/bin/env python3
 import sys
 
 from puroboros.context import Context
-from puroboros.defs import Token, TokenType
+from puroboros.expr import Parser
 from puroboros.scan import Scanner
-
-
-tok_str = {
-    TokenType.T_PLUS: '+',
-    TokenType.T_MINUS: '-',
-    TokenType.T_STAR: '*',
-    TokenType.T_SLASH: '/',
-    TokenType.T_INTLIT: 'intlit'
-}
-
-
-def scanfile(context: Context) -> None:
-    scanner = Scanner(context)
-    token = Token()
-    while scanner.scan(token):
-        print(f'Token {tok_str[token.type]}', end='')
-        if (token.type == TokenType.T_INTLIT):
-            print(f', value {token.intvalue}', end='')
-        print()
+from puroboros.interp import interpret_ast
 
 
 if __name__ == '__main__':
-    context = Context()
-
     with open(sys.argv[1], 'rt') as fi:
+        context = Context()
         context.infile = fi
-        scanfile(context)
+        scanner = Scanner(context)
+        parser = Parser(scanner)
+
+        node, _ = parser.bin_expr()
+    print(interpret_ast(node))
