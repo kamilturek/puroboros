3dfff4cb7af7b63cfab0c168bd7bcdbea782b4db
Kamil Turek <kamil.turek@hotmail.com>
2021-05-01 13:34:19 +0200

    Use argparse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import sys
+import argparse
+import platform
 
 from puroboros.context import Context
 from puroboros.expr import Parser
@@ -7,15 +8,35 @@ from puroboros.gen import CodeGenerator
 from puroboros.scan import Scanner
 
 
-if __name__ == '__main__':
-    with open(sys.argv[1], 'rt') as fi:
+def compile(args):
+    with open(args.file, 'rt') as infile:
         context = Context()
-        context.infile = fi
+        context.infile = infile
         scanner = Scanner(context)
         parser = Parser(scanner)
-
         node, _ = parser.bin_expr()
 
-    generator = CodeGenerator('darwin', 'arm64')
+    generator = CodeGenerator(args.system, args.arch)
     generator.generate(node)
-    print(generator.assembly.outstream.getvalue())
+
+    with open(args.output, 'wt') as outfile:
+        outfile.write(generator.assembly.output)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Puroboros C compiler')
+    parser.add_argument('file', type=str, help='input path')
+    parser.add_argument('-o', '--output', type=str, help='output path', default='a.out')
+    parser.add_argument(
+        '-s',
+        '--system',
+        type=str,
+        help='operating system name',
+        default=platform.system(),
+    )
+    parser.add_argument(
+        '-a', '--arch', type=str, help='architecture type', default=platform.machine()
+    )
+    args = parser.parse_args()
+
+    compile(args)
