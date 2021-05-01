fdbfc479bba534fb133b5b5b85f066f4df17b6fc
Kamil Turek <kamil.turek@hotmail.com>
2021-05-01 11:51:21 +0200

    Test CodeGenerator

--- puroboros/gen.py
+++ puroboros/gen.py
@@ -1,17 +1,20 @@
 from puroboros.defs import ASTNode
-from puroboros.codegen.darwin.arm64 import DarwinARM64CodeGenerator
+from puroboros.asm.darwin.arm64 import DarwinARM64
 from puroboros.exceptions import CodeGenerationError
 
 
-class Generator:
+class CodeGenerator:
     def __init__(self) -> None:
         # use some factory here to get proper generator
-        self.generator = DarwinARM64CodeGenerator()
+        self.assembly = self._get_assembly()
 
     def generate(self, node: ASTNode) -> None:
-        self.generator.preamble()
+        self.assembly.preamble()
         self._generate_ast(node)
-        self.generator.postamble()
+        self.assembly.postamble()
+
+    def _get_assembly(self):
+        return DarwinARM64()
 
     def _generate_ast(self, node: ASTNode) -> None:
         if node.left:
@@ -21,16 +24,15 @@ class Generator:
 
         match node.op:
             case ASTNode.Type.A_ADD:
-                return self.generator.add(left_reg, right_reg)
+                return self.assembly.add(left_reg, right_reg)
             case ASTNode.Type.A_SUBTRACT:
-                return self.generator.sub(left_reg, right_reg)
+                return self.assembly.sub(left_reg, right_reg)
             case ASTNode.Type.A_MULTIPLY:
-                return self.generator.mul(left_reg, right_reg)
+                return self.assembly.mul(left_reg, right_reg)
             case ASTNode.Type.A_DIVIDE:
-                return self.generator.div(left_reg, right_reg)
+                return self.assembly.div(left_reg, right_reg)
             case ASTNode.Type.A_INTLIT:
-                return self.generator.load(node.intvalue)
+                return self.assembly.load(node.intvalue)
             case _:
                 msg = f'Unknown AST operator {node.op}'
                 raise CodeGenerationError(msg)
-            
